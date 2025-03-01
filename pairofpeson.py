26dd61b8c005a21025a416a5f2d34ee2ad5856df
andrewcgithub <66339255+andrewcgithub@users.noreply.github.com>
2025-02-28 22:34:45 -0800

    Update pairofpeson.py

--- pairofpeson.py
+++ pairofpeson.py
@@ -1,28 +1,32 @@
-def getMinSwaps(row):
-    swaps = 0 # Initialize the function recording the amount of swaps taking place.
-    # For loop iterates through the row and then performs a swap when a number is out of place.
+# This function will return the minimum amount of swaps performed to arrange 'couples' in seats next to each other.
+def getMinimumSwaps(row):
+    #Variables:
+    minimumSwaps = 0        # (Int) Minimum number of swaps returned by the function. Incremented in-loop.
+
+    #The for loop will iterate through the table "row" from index 0 to the index representing the length of the array
     for i in range(0, len(row), 2):
-        # Check if the next number represents the spouse
-        potentialSpouse = row[i+1]
-        # If the number next to row[i] is NOT the spouse (+1 to row[i]'s value)
-        if row[i]+1 != row[i+1]:
-            # Loop through row items starting from i+1 and ending at the row's length - index position + 1
-            for j in range(i, (len(row))):
-                # If the row's value is equal to row[i]'s value incremented by 1
-                if row[j] == row[i]+1:
-                    swaps+=1                        # Increment swaps
-                    potentialSpouse = row[j]        # Set potential spouse to row[j]
-                    row[j] = row[i+1]               # Swap row[j] with row[i+1]
-                    row[i+1] = potentialSpouse      # Set row[i+1] to the value of the original row[j]
-        
-    return swaps
+        # row[i] represents individual one, and row[i+1] represents individual two.
+        # Because couples come paired starting at (0,1),(2,3),etc, the number can be divided by 2 and rounded
+        # to the lower number in order to be considered a match for row[i].
+        if row[i] // 2 != row[i + 1] // 2:
+            # If row[i+1] is not a couples match for row[i], then we shall iterate through the table again starting
+            # from position i+1
+            for j in range(i + 1, len(row)):
+                # The if statement checks if the number found matches the original as a member of the 'couple'.
+                if row[j] // 2 == row[i] // 2:
+                    # The numbers are swapped and minimumSwaps is incremented by 1
+                    temp = row[i+1]
+                    row[i+1] = row[j]
+                    row[j] = temp
+                    minimumSwaps += 1
+    return minimumSwaps
     
 
 
 def main():
     row = [0, 2, 1, 3]
-    minSwaps = getMinSwaps(row)
-    print("Minimum number of swaps with row: "+str(minSwaps))
+    minimumSwaps = getMinimumSwaps(row)
+    print("Minimum number of swaps with row: "+str(minimumSwaps))
 
 if __name__ == "__main__":
     main()
